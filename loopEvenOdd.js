4a0ace281816b2f413cc5c1fc7b5f5f5cafd4000
Harry <hmsh2000@gmail.com>
2025-01-22 20:42:56 +0000

    loopEvenOdd

---
+++ loopEvenOdd.js
@@ -0,0 +1,9 @@
+// Write a for loop iterating from 1 to 20 and logs whether each number is "even" or "odd".
+
+for (let i = 1; i < 20; i++) {
+  if (i % 2 === 0) {
+    console.log(`"${i} is even"`);
+  } else {
+    console.log(`"${i} is odd"`);
+  }
+}
