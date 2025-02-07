8ca48ed841bdeaab044449b1879ec50ffd07f9cb
Harry <hmsh2000@gmail.com>
2025-02-07 16:56:56 +0000

    mapping

--- map.js
+++ map.js
@@ -11,12 +11,12 @@
 
 // Square Numbers in an array
 const numbers = [1, 2, 3, 4, 5];
+
 const squaredNumbers = numbers.map((num) => num * num);
 console.log(squaredNumbers);
 
 // Convert strings in array to uppercase
 const words = ["hello", "world", "javascript"];
-
 const uppercaseWords = words.map((word) => word.toUpperCase());
 console.log(uppercaseWords);
 
@@ -41,19 +41,45 @@ const discountedProducts = products.map((product) => ({
   name: product.name,
   price: product.price * 0.9,
 }));
-
 console.log(discountedProducts);
 
 // Write a function taking an array of numbers and returning a new array where each number is doubled
 
 function doubleNumber(numbers) {
-  return numbers.map((num) => num * 2);
+  return numbers.map((number) => number * 2);
 }
 console.log(doubleNumber([1, 2, 3, 4]));
 
 // Write a function taking an array of strings and returning a new array with '!' added to end of each string
 
-function addExclamation(strArray) {
-  return strArray.map((word) => word + "!");
+function addExclamation(words) {
+  return words.map((word) => word + "!");
 }
 console.log(addExclamation(["hello", "world"]));
+
+// Given an array of objects representing people, return an array of their ages
+
+const people = [
+  { name: "John", age: 22 },
+  { name: "Sarah", age: 30 },
+  { name: "Mike", age: 25 },
+];
+
+function extractAges(people) {
+  return people.map((people) => people.age);
+}
+
+console.log(extractAges(people));
+
+// Given an array of product objects with 'price', return a new array where prices are formatted as strings prefixed with '£'
+
+const items = [
+  { name: "TV", price: 300 },
+  { name: "Laptop", price: 1000 },
+];
+
+function formatPrices(items) {
+  return items.map((item) => "£" + item.price);
+}
+
+console.log(formatPrices(items));
