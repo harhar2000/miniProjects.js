f2e2c79835aa342e09e27b0530ed816b7233c6da
Harry <hmsh2000@gmail.com>
2025-02-10 10:07:42 +0000

    Promise

---
+++ promise.js
@@ -0,0 +1,20 @@
+// Promises handle asynchronicity and regain control over the execution flow of the programme
+// They let you control the order in which some operations will run in the code
+//  e.g. If your code depends on external server data, promises let you run these operations
+//        once you have received the data
+
+// Promises are objects - containers for future data, a representation of future completion or failure
+//   of an asynchronous operation. As such, they have three different states:
+
+//  Fulfilled - asynchronous operation completed successfully:
+//              for instance, data you requested from an external server came back with no isses.
+
+//  Rejected - asynchronous operation failed:
+//             for instance, you sent an invalid request to a server and it answered with an error.
+
+//  Pending - the Promise has yet to to be either rejected or fulfilled:
+//            the lapse between sending a request and getting an answer back from your server.
+
+const promise = new Promise((resolve, reject) => {
+  pass;
+});
